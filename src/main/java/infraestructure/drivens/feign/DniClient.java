6bd427a56404250b4a33e5315eb1d59fa650ac33
AngelMoraZazoDecath <angelmaria.mora@decathlon.com>
2025-04-30 01:08:53 +0200

    change directories

---
+++ src/main/java/infraestructure/drivens/feign/DniClient.java
@@ -0,0 +1,35 @@
+package infraestructure.drivens.feign;
+
+import application.dto.CheckDniRequest;
+import application.dto.CheckDniResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+ 
+import feign.Logger;
+import feign.okhttp.OkHttpClient;
+ 
+@FeignClient(name = "checkDniClient", url = "${external.service.url}", configuration = DniClient.FeignConfig.class)
+public interface DniClient {
+
+  @Configuration
+  public class FeignConfig {
+ 
+    @Bean(name = "customOkHttpClient")
+    public OkHttpClient okHttpClient() {
+      return new OkHttpClient();
+    }
+ 
+    @Bean(name = "customLogger")
+    public Logger.Level feignLoggerLevel() {
+      return Logger.Level.FULL; // Puedes ajustar el nivel de los logs según tu necesidad
+    }
+  }
+ 
+  @PatchMapping(value = "/check-dni")
+  ResponseEntity<CheckDniResponse> check(@RequestBody CheckDniRequest request);
+}
+ 
