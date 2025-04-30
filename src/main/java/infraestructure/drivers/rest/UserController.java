89713376181f9ab57a4925b90a065bc343c40fca
AngelMoraZazoDecath <angelmaria.mora@decathlon.com>
2025-04-30 02:01:36 +0200

    create controller

---
+++ src/main/java/infraestructure/drivers/rest/UserController.java
@@ -0,0 +1,26 @@
+package infraestructure.drivers.rest;
+
+import application.dto.user.CreateUserRequest;
+import application.dto.user.CreateUserResponse;
+import application.ports.in.CreateUserUseCase;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class UserController {
+
+    private final CreateUserUseCase createUserUseCase;
+
+    @PostMapping("/user")
+    public ResponseEntity<CreateUserResponse> createUser(@RequestBody CreateUserRequest request) {
+        CreateUserResponse response = createUserUseCase.createUser(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+
+}
